ec724be51386548d009884faf8a031f976b5c666
Katerina jser <kmalceva148@gmail.com>
2024-09-26 19:38:19 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Review from "./Review";
+import { AiOutlineGithub } from "react-icons/ai";
+
+function App() {
+  return (
+    <div>
+      <h2>
+        Reviews Project Setup
+        <AiOutlineGithub />
+      </h2>
+      <Review />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    job: "Web Developer",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    job: "Graphic Designer",
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+    text: "Vestibulum viverra tellus ut augue bibendum, a tincidunt magna venenatis.",
+  },
+  // Додайте інші користувацькі дані тут
+];
+
+export default people;
